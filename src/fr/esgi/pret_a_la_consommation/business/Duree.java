dfd6fa1b2e26c960a500d40e01159b3147b4e3bb
Alexis6942069 <alexis.barreyre@gmail.com>
2023-02-07 17:52:45 +0100

    Durée

--- src/fr/esgi/pret_a_la_consommation/business/Duree.java
+++ src/fr/esgi/pret_a_la_consommation/business/Duree.java
@@ -6,4 +6,22 @@ public class Duree {
     private long id;
     private int dureeEnMois;
     private static long compteur = 0L;
+
+    public Duree(long id)
+    {
+        this.id = ++compteur;
+    }
+
+    /**
+     *
+     * @return id du prêt : long
+     */
+    public long getId() { return id; } // id du prêt
+
+    /**
+     *
+     * @return (en mois) : int
+     */
+    public int getDureeEnMois() { return dureeEnMois; }
+
 }
