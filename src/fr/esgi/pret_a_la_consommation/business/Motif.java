4932bbacfaed3017b0a40126fa9723b9bf3776c9
valentyn <coudiere.valentyn@gmail.com>
2023-02-07 13:00:44 +0100

    Classe Motif

--- src/fr/esgi/pret_a_la_consommation/business/Motif.java
+++ src/fr/esgi/pret_a_la_consommation/business/Motif.java
@@ -1,9 +1,42 @@
 package fr.esgi.pret_a_la_consommation.business;
 
 public class Motif {
+
     //attributs
     private long id;
     private String nom;
     private String description;
     private static long compteur = 0L;
+
+    //getters
+    public long getId() {
+        return id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public static long getCompteur() {
+        return compteur;
+    }
+
+    //setters
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public static void setCompteur(long compteur) {
+        Motif.compteur = compteur;
+    }
+
+    public Motif() {
+        id = ++compteur;
+    }
 }
