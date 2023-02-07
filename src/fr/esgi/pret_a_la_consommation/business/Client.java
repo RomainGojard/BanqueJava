0151c020217800cf562164ddb9d056f8dc8979cb
romaingojard <romain.gojard@gmail.com`>
2023-02-07 16:34:36 +0100

    Set up Client

--- src/fr/esgi/pret_a_la_consommation/business/Client.java
+++ src/fr/esgi/pret_a_la_consommation/business/Client.java
@@ -1,12 +1,34 @@
 package fr.esgi.pret_a_la_consommation.business;
 
-public class Client {
-    //attributs
+import java.util.Comparator;
+
+public class Client implements Comparator<Client> {
+    /**
+     * Classe qui définit les clients de notre banque
+     * @author romaingojard
+     */
 
     private long id;
     private String nom;
     private String prenom;
     private static long compteur = 0L;
 
+    public Client(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.id = ++compteur;
+    }
 
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    @Override
+    public int compare(Client client1, Client client2) {
+        return client1.getNom().compareTo(client2.getNom());
+    }
 }
