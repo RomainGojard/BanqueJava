b083998957c41f1575d87bac3c3f1fa48774e530
Romain <romain.gojard@gmail.com>
2023-02-10 13:00:49 +0100

    Implement MontantExcessifEception

--- src/fr/esgi/pret_a_la_consommation/exceptions/MontantExcessifException.java
+++ src/fr/esgi/pret_a_la_consommation/exceptions/MontantExcessifException.java
@@ -1,7 +1,16 @@
 package fr.esgi.pret_a_la_consommation.exceptions;
 
-public class MontantExcessifException {
+public class MontantExcessifException extends RuntimeException {
 
     // TODO: 09/02/2023 Exception levée lorsque le montant demandé par l'utilisateur pour créer un prêt est >= 20 000
-    
+
+    /**
+     *
+     * @param message
+     * @author Y-Vector
+     */
+    public MontantExcessifException (String message) {
+        super(message);
+    }
+
 }
--- src/fr/esgi/pret_a_la_consommation/service/impl/PretServiceImpl.java
+++ src/fr/esgi/pret_a_la_consommation/service/impl/PretServiceImpl.java
@@ -3,6 +3,7 @@ package fr.esgi.pret_a_la_consommation.service.impl;
 import fr.esgi.pret_a_la_consommation.business.Client;
 import fr.esgi.pret_a_la_consommation.business.Pret;
 import fr.esgi.pret_a_la_consommation.business.Taux;
+import fr.esgi.pret_a_la_consommation.exceptions.MontantExcessifException;
 import fr.esgi.pret_a_la_consommation.service.PretService;
 
 import java.time.LocalDate;
@@ -12,8 +13,20 @@ import java.util.ArrayList;
 public class PretServiceImpl implements PretService {
     private static ArrayList<Pret> prets = new ArrayList<>();
 
+
+    /**
+     *
+     * @param pret
+     * @return
+     * @throws MontantExcessifException
+     * @author Y-Vector
+     */
     @Override
-    public Pret ajouterPret(Pret pret) {
+    public Pret ajouterPret(Pret pret) throws MontantExcessifException {
+
+        if (pret.getMontantMensualite() >= 20000) {
+            throw new MontantExcessifException("Le prêt ne doit pas excéder 20000 euros.");
+        }
         prets.add(pret);
         return pret;
     }
@@ -21,17 +34,16 @@ public class PretServiceImpl implements PretService {
     @Override
     public Pret ajouterPret(long montantDemande, LocalDateTime dateSouscription, String observations, Client client, Taux taux) {
 
-        double interetAnnuel = (taux.getValeur()/100);
+        double interetAnnuel = (taux.getValeur() / 100);
         double interetMensuel = interetAnnuel / 12;
 
         double montantMensualite = (montantDemande * interetMensuel) / (1 - Math.pow((1 + interetMensuel), -taux.getDuree().getDureeEnMois()));
 
         LocalDate dateEffet = null;
 
-        Pret pret = new Pret(montantDemande, montantMensualite, dateSouscription, dateEffet, observations, client, taux );
+        Pret pret = new Pret(montantDemande, montantMensualite, dateSouscription, dateEffet, observations, client, taux);
         return ajouterPret(pret);
     }
-
 
 
 }
